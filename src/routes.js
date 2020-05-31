50d3cfaf8c3c0ec516600747411e88708e2b4c0e
Luiz Fernando de Morais <luiz.ado@hotmail.com>
2020-05-31 11:23:00 -0300

    Starting the project

---
+++ src/routes.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const routes = express.Router()
+const Instructor = require('../src/app/controllers/Instructor')
+const Member = require('../src/app/controllers/Member')
---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const server = express()
+const routes = require('./routes')
+const nunjucks = require('nunjucks')
+const methodOverride = require('method-override')
+
+
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static('public'))
+server.use(methodOverride('_method'))
+server.use(routes)
+
+
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+
+
+server.listen('5000', function() {
+    console.log('server is running')
+})
