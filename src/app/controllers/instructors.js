f7c5c96c84fa90b63e9252adf8312aecd1d135e6
Luiz Fernando de Morais <luiz.ado@hotmail.com>
2020-06-01 23:33:15 -0300

    Show view and PostgreSQL queries

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,8 +1,36 @@
+const Instructor = require("./models/Instructor")
+const { date, age } = require("../lib/utils")
+
 module.exports = {
     index(req, res) {
         return res.render('instructors/index')
     },
     create(req, res) {
         return res.render('instructors/create')
+    },
+    post(req, res) {
+        const keys = Object.keys(req.body)
+
+        for(key of keys) {
+            if(req.body[key] == "") {
+                return res.send('Please, fill all the fields!')
+            }
+        }
+
+        Instructor.create(req.body, function(instructor) {
+            return res.redirect(`/instructors/${instructor.id}`)
+        })
+    },
+    show(req, res) {
+        Instructor.find(req.params.id, function(instructor) {
+            if(!instructor) return res.send("Instructor not found")
+
+            instructor.age = age(instructor.birth)
+            instructor.services = instructor.services.split(",")
+
+            instructor.created_at = date(instructor.created_at).format
+
+            return res.render("./instructors/show", { instructor })
+        })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const routes = express.Router()
-const Instructors = require('./app/controllers/Instructors')
-const Members = require('./app/controllers/Members')
+const instructors = require('./app/controllers/instructors')
+const Members = require('./app/controllers/members')
 const Home = require('./app/controllers/Home')
 
 routes.get('/', Home.index)
 
 /* ===== instructors ===== */
 
-routes.get('/instructors', Instructors.index)
-routes.get('/instructors/create', Instructors.create)
+routes.get('/instructors', instructors.index)
+routes.get('/instructors/create', instructors.create)
+routes.get('/instructors/:id', instructors.show)
+
+
+routes.post("/instructors", instructors.post)
 /* ===== members ====== */
 
 routes.get('/members', Members.index)
